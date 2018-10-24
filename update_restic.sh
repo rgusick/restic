5c444a566f88c9db4c5ff9a9cdb25be892f4093b
rgusick <robert@gusick.com>
2018-10-24 09:12:04 -0400

    Create update_restic.sh
    Fetch the latest version of restic

---
+++ update_restic.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+RESTIC_TAG_LATEST=$(curl --silent "https://api.github.com/repos/restic/restic/releases/latest" | grep -Po '"tag_name": "v\K.*?(?=")')
+echo "Downloading and installing restic v$RESTIC_TAG_LATEST ..."
+RESTIC_URL=https://github.com/restic/restic/releases/download/v${RESTIC_TAG_LATEST}/restic_${RESTIC_TAG_LATEST}_linux_amd64.bz2
+curl -L --silent ${RESTIC_URL} | bunzip2 > /usr/local/bin/restic
+chmod 755 /usr/local/bin/restic
