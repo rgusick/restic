361f8d5c064ecdbd9472d12622dbc1b9b3401779
rgusick <robert@gusick.com>
2018-10-24 09:13:29 -0400

    Update update_restic.sh
    Change path for executable

--- update_restic.sh
+++ update_restic.sh
@@ -3,5 +3,5 @@
 RESTIC_TAG_LATEST=$(curl --silent "https://api.github.com/repos/restic/restic/releases/latest" | grep -Po '"tag_name": "v\K.*?(?=")')
 echo "Downloading and installing restic v$RESTIC_TAG_LATEST ..."
 RESTIC_URL=https://github.com/restic/restic/releases/download/v${RESTIC_TAG_LATEST}/restic_${RESTIC_TAG_LATEST}_linux_amd64.bz2
-curl -L --silent ${RESTIC_URL} | bunzip2 > /usr/local/bin/restic
-chmod 755 /usr/local/bin/restic
+curl -L --silent ${RESTIC_URL} | bunzip2 > /usr/bin/restic
+chmod 755 /usr/bin/restic
